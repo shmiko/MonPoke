e6b5848556a922089b6714f3df1542360297b5c4
shmiko <shmiko@gmail.com>
2016-09-18 18:11:31 +1000

    AR Pokemon Card

---
+++ Assets/moltresScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class moltresScript : MonoBehaviour {
+
+	public Button fire2Button;
+
+	public GameObject spawnPoint;
+
+	public GameObject fireBall;
+
+	// Use this for initialization
+
+	void Start () {
+
+		fire2Button.onClick.AddListener (fire2ButtonDown);
+
+	}
+
+	void fire2ButtonDown(){
+
+		fireBall = Instantiate (Resources.Load ("FireMobile"), spawnPoint.transform.position, Quaternion.identity) as GameObject;
+
+		fireBall.GetComponent<Rigidbody> ().AddRelativeForce (spawnPoint.transform.forward * -1000f);
+
+		Destroy (fireBall, 2);
+
+	}
+
+	// Update is called once per frame void Update () { } }
+}
